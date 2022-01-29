a2c1c5aa31ccfe98b53471ae96a559bdf3de7d8f
annie950303 <annie.yeh2006@gmail.com>
2022-01-29 18:52:14 +0800

    1/29-1

--- APCSCamp 語法班/213.cpp
+++ APCSCamp 語法班/213.cpp
@@ -1,41 +1,40 @@
 #include<iostream>
 using namespace std;
 
-bool nmk[55][55][55];
-const int MX[] = {0, 0, 0, 0, 1, -1}, MY[] = {0, -1, 0, 1, 0, 0}, MZ[] = {1, 0, -1, 0, 0, 0};
+bool nmk[50][50][50];
+const int MX[] = {1, -1, 0, 0, 0, 0}, MY[] = {0, 0, 1, -1, 0, 0}, MZ[] = {0, 0, 0, 0, 1, -1};
 
 int main(){
-	int n, m, k;// n - x, m - y, k - z;
+	int n, m, k;
 	cin >> n >> m >> k;
-	for ( int i = 0; i < n; i++ ){
-		for ( int j = 0; j < m; j++ ){
-			for ( int l = 0; l < k; l++ ){
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < m; j++){
+			for (int l = 0; l < k; l++){
 				cin >> nmk[i][j][l];
 			}
 		}
 	}
+	int x = 0, y = 0, z = 0;
+	int nx, ny, nz;
 	bool can_walk = true;
-	int x = 0, y = 0, z = 0, nx, ny, nz;
-	while ( can_walk ){
+	while (can_walk){
 		can_walk = false;
-		for ( int i = 0; i < 6; i++ ){
+		for (int i = 0; i < 6; i++){
 			nx = x + MX[i];
 			ny = y + MY[i];
 			nz = z + MZ[i];
-			cout << '*' << nx << ' ' << ny << ' ' << nz << '\n';
-			if ( nx < 0 || nx == n || ny < 0 || ny == m || nz < 0 || nz == k ){
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= k){
 				continue;
 			}
-			if ( !nmk[x][y][z] ){
+			else if (!nmk[nx][ny][nz]){
 				can_walk = true;
-				nmk[x][y][z] = true;
+				nmk[nx][ny][nz] = true;
 				x = nx;
 				y = ny;
 				z = nz;
 				break;
 			}
 		}
-		cout << ' ' << x << ' ' << y << ' ' << z << '\n';
 	}
 	cout << x << ' ' << y << ' ' << z << '\n';
 	return 0;
